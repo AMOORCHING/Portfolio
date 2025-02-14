e8b5d8ff544a47497f713f72c9c90d727d06833e
akash <58539815+AMOORCHING@users.noreply.github.com>
2025-02-14 03:13:36 -0500

    refactor: layouts, mobile

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -1,12 +1,12 @@
-import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-import Layout from "../components/Layout";
+import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <Layout>
-      <Navbar />
-      <Hero />
-    </Layout>
+    <div>
+        <Navbar />
+        <Hero />
+    </div>
   );
 }
