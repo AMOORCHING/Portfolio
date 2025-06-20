30f0aefca90bd3b4002288528e9a92f195a957e2
akash <58539815+AMOORCHING@users.noreply.github.com>
2025-06-19 22:54:03 -0500

    fix: mobile footer

--- my-project/src/components/Footer.tsx
+++ my-project/src/components/Footer.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="w-full bg-white border-t border-gray-200 p-4 flex justify-between items-center font-supply-mono text-sm">
-      <div className="text-gray-600">
+    <footer className="w-full bg-white border-t border-gray-200 p-4 flex flex-col sm:flex-row sm:justify-between items-center font-supply-mono text-sm gap-2">
+      <div className="text-gray-600 text-center sm:text-left">
         Thanks for visiting!
       </div>
-      <div className="text-gray-600">
+      <div className="text-gray-600 text-center sm:text-right">
         (C) Akash Moorching. All rights reserved.
       </div>
     </footer>
